781e89f457c8d26445af3edb6b0f812335dd7390
Jatinder Kumar Chaurasia <jatinderkumarchaurasia@gmail.com>
2020-04-18 14:46:13 +0530

    EmbeddedStructTest

--- pluralsight_course/custom_datatypes_go/organization/person.go
+++ pluralsight_course/custom_datatypes_go/organization/person.go
@@ -16,6 +16,11 @@ type Handler struct {
 	name   string
 }
 
+type Name struct {
+	firstName string
+	lastName  string
+}
+
 // type TwitterHandler = string // type alias // copy the fields and method sets // not able to extend the type
 type TwitterHandler string // type declaration => copy the fields and the objects
 
@@ -25,8 +30,7 @@ func (handler TwitterHandler) RedirectURL() string {
 }
 
 type Employee struct {
-	firstName string
-	lastName  string
+	Name
 }
 
 // Struct
@@ -36,8 +40,10 @@ type Person struct {
 	//LastName  string
 
 	// private mode
-	firstName      string
-	lastName       string
+	//firstName      string
+	//lastName       string
+	//name Name
+	Name
 	twitterHandler TwitterHandler
 }
 
@@ -45,13 +51,16 @@ type Person struct {
 //so ...
 func NewPerson(firstName, lastName string) *Person {
 	return &Person{
-		firstName: firstName,
-		lastName:  lastName,
+		//name: Name{firstName: firstName,lastName: lastName},
+		Name: Name{firstName: firstName, lastName: lastName},
 	}
 }
 
-func (person *Person) FullName() string {
-	return fmt.Sprintf("%s %s", person.firstName, person.lastName)
+//func (person *Person) FullName() string {
+func (name *Name) FullName() string {
+	//return fmt.Sprintf("%s %s", person.name.firstName, person.name.lastName)
+	return fmt.Sprintf("%s %s", name.firstName, name.lastName)
+
 }
 
 func (person *Person) ID() string {
